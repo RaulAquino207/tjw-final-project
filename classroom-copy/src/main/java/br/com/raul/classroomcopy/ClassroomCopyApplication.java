94d37008d893bbdbe118fb89b3b68395cce7e780
Raul Aquino <aquinoraul207@gmail.com>
2022-06-27 00:45:19 -0300

    added keycloak along with initial project

---
+++ classroom-copy/src/main/java/br/com/raul/classroomcopy/ClassroomCopyApplication.java
@@ -0,0 +1,13 @@
+package br.com.raul.classroomcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomCopyApplication.class, args);
+	}
+
+}
---
+++ classroom-copy/src/test/java/br/com/raul/classroomcopy/ClassroomCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raul.classroomcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
